d4d9165245082ddbb62312bf35feef4215d1ad4d
Zigmantas <zigmantas@mrorangetech.se>
2021-09-20 18:29:47 +0300

    pridetas 1.2 nd

---
+++ Homework/uzduotis.1.2/uzduotis.1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uzduotis._1._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Įveskite atstumą metrais.");
+			double atstumas = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Įveskite laiką sekundėmis.");
+			double laikas = Convert.ToDouble(Console.ReadLine());
+
+			double atstumasKilometrais = atstumas / 1000;
+			double laikasValandomis = laikas / 3600;
+
+			double greitis = atstumasKilometrais / laikasValandomis;
+
+			Console.WriteLine($"Jūsų greitis yra {greitis} km/h.");
+		}
+	}
+}
---
+++ Homework/uzduotis1.2/uzduotis1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uzduotis1._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Iveskite atstuma metrais");
+			double atstumas = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Iveskite laika sekundemis");
+			double laikas = Convert.ToDouble(Console.ReadLine());
+
+			double atstumasKm = atstumas / 1000;
+			double laikasValandomis = laikas / 3600;
+
+			double greitis = atstumasKm / laikasValandomis;
+
+			Console.WriteLine($"Jūsų greitis yra {greitis} km/h.");
+
+		}
+
+	}
+}
