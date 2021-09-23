94c9d53c46ef21435039f6b1527bbaf373361e7a
Zigmantas <zigmantas@mrorangetech.se>
2021-09-23 18:07:29 +0300

    prideta 1.3 uzduotis

---
+++ Homework/uzduotis1.3/uzduotis1.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uzduotis1._3_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Įveskite vardą.");
+			var name = Console.ReadLine();
+
+			Console.WriteLine("Įveskite pavardę.");
+			var surname = Console.ReadLine();
+
+			Console.WriteLine("Įveskite gimimo vietą.");
+			var place = Console.ReadLine();
+
+			Console.WriteLine(name + " " + surname + " deginasi mieste, kuris vadinasi " + place);
+		}
+	}
+}
