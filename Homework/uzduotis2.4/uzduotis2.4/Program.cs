1e377882849362c409d4b7c2e8f12036e72e6b57
Zigmantas <zigmantas@mrorangetech.se>
2021-09-27 18:35:47 +0300

    prideta 2.4

---
+++ Homework/uzduotis2.4/uzduotis2.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uzduotis2._4_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Sveiki! Ši išspausdins skaičius nuo 0 iki 20 kas trečią dviem algortimais.");
+
+			Console.WriteLine("Pirmas algortimas:");
+
+			for(int i = 0; i < 20; i++)
+			{
+				if(i % 3 == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+
+			Console.WriteLine("Antras algortimas:");
+
+			for(int i = 0; i < 20; i += 3)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
