79386fbf5d73c9438bf77b8691e05196d7817ca4
Zigmantas <zigmantas@mrorangetech.se>
2021-09-28 18:13:43 +0300

    3.1. uzduotis

---
+++ Homework/uzduotis3.1.antras dublis/uzduotis3.1.antras dublis/Controllers/IntervalController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uzduotis3._1.antras_dublis.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IntervalController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get(
+			int start,
+			int end)
+		{
+			if (start > end)
+			{
+				return new OkObjectResult("Blogi rėžiai");
+			}
+			else
+			{
+				var message = "";
+
+				for(int i = start; i <= end; i++)
+				{
+					message += $"{i} {i * i} \n";
+				}
+
+				return new OkObjectResult(message);
+			}
+		}
+	}
+}
---
+++ Homework/uzduotis3.1/uzduotis3.1/Controllers/IntervalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace uzduotis3._1_.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IntervalController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get(
+			int start, 
+			int end)
+		{
+			if (start > end)
+			{
+				return new OkObjectResult("Blogi rėžiai");
+			}
+
+			return Ok();
+		}
+	}
+}
