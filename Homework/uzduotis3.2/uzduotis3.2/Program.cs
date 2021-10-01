2a5de361d253945af1cf49f4a13b79fc363374dd
Zigmantas <zigmantas@mrorangetech.se>
2021-10-01 18:15:03 +0300

    uzduotis 3.2

---
+++ Homework/uzduotis3.2/uzduotis3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uzduotis3._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string choice = "";
+
+			Console.WriteLine("Pasirnkite: \n 1 - laptopas; \n 2 - stacionarus;");
+
+			while(choice != "1" && choice != "2")
+			{
+				if(choice != "")
+				{
+					Console.WriteLine("Neteisingas pasirinkimas. bandykite dar kartą.");
+				}
+
+				choice = Console.ReadLine();
+			}
+
+			var message = choice == "1" ? "laptopą" : "stacionarą";
+
+			Console.WriteLine($"Jus pasirinkote {message}");
+		}
+	}
+}
