32087ef1b4893cb1320347cd20b3a10ccaabe116
Zigmantas <zigmantas@mrorangetech.se>
2021-10-08 18:12:30 +0300

    prideta uzduotis

---
+++ uzduotis4.2_duplis2/uzduotis4.2_duplis2/Controllers/NamesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace uzduotis4._2_duplis2.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NamesController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult ProcessNames(string[] names)
+		{
+			var nameList = new List<string>();
+
+			foreach (var name in names)
+			{
+				var length = name.Length;
+
+				nameList.Add($"{name} vardo ilgis yra {length} simboliai");
+			}
+
+			return new OkObjectResult(nameList);
+		}
+	}
+}
